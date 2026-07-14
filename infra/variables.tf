659d031a2e5fbe50d0adcec8474d5e329610a579
ysman <tke0329@gmail.com>
2026-07-14 09:40:50 +0900

    Add Terraform infrastructure

---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+      description = "region"
+      default = "ap-northeast-2"
+}
+
+variable "prefix" {
+      description = "Prefix for all resources"
+      default = "terra"
+}
+
+variable "app_1_domain" {
+  description = "app_1 domain"
+  default     = "api.p-14832-1.oa.gg"
+}
